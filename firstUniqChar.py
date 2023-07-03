ad9ee192b479879f385dcc371f3c9fee3f00b471
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2023-07-03 13:43:42 +0530

    ..

--- firstUniqChar.py
+++ firstUniqChar.py
@@ -1,3 +1,28 @@
+# First Unique Character in a String
+
+# Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+# Example 1:
+# Input: s = "leetcode"
+# Output: 0
+
+# Example 2:
+# Input: s = "loveleetcode"
+# Output: 2
+
+# Example 3:
+# Input: s = "aabb"
+# Output: -1
+
+# Constraints:
+# a. 1 <= s.length <= 10^5
+# b. s consists of only lowercase English letters.
+
+# Note: Create a GitHub file for the solution and add the file link the the answer section below.
+# 5 points
+# https://github.com/farazsfa007/Full-Stack-Data-Science/blob/master/firstUniqChar.py
+
+
 def firstUniqChar(s):
     char_count = {}  # Dictionary to store character count
 
